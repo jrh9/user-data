3d70c8f794aaf37341eb21a4abd20713d58616ed
Jon <jon.hernandez93@gmail.com>
2017-10-08 13:48:24 -0700

    Implement User model

--- src/main/java/com/jrh/userdata/model/User.java
+++ src/main/java/com/jrh/userdata/model/User.java
@@ -1,4 +1,53 @@
 package com.jrh.userdata.model;
 
 public class User {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String name, String email) {
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email= email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+    }
+
 }
