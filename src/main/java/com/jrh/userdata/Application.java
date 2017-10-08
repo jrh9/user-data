4f7b9d8f6e5e29847155916a7619a3d29ba6a819
Jon Hernandez <jon.hernandez93@gmail.com>
2017-10-08 12:42:22 -0700

    Modify Application.java to run Spring Application

--- src/main/java/com/jrh/userdata/Application.java
+++ src/main/java/com/jrh/userdata/Application.java
@@ -3,6 +3,14 @@ package com.jrh.userdata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 
 public class Application {
+
+    //private static final Class<Application> applicationClass = Application.class;
+    //private static final Logger log = LoggerFactory.getLogger(applicationClass);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
